ef77ce8e505a066afba08112d60d495ddbdb86cf
richardli1598 <opennaslab@gmail.com>
2024-03-10 14:18:43 +0800

    ci: add ci to test deploying
    Signed-off-by: richardli1598 <opennaslab@gmail.com>

--- hack/local-start.sh
+++ hack/local-start.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+export KO_DOCKER_REPO=${KO_DOCKER_REPO:-localhost:5000}
+
+source ./hack/utils.sh
+
 # 1.Verify env
 if [[ -z ${KO_DOCKER_REPO} ]]; then
     echo "KO_DOCKER_REPO is not set. Please set it to the docker repository where you want to store the image."
@@ -11,10 +15,11 @@ fi
 # 2.Install go env
 if [[ -z $(which go) ]]; then
     echo "go is not installed. Try to install go..."
-    wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz -P /tmp/
-    tar -C /usr/local -xzf /tmp/go1.22.0.linux-amd64.tar.gz
-    rm -rf /tmp/go1.22.0.linux-amd64.tar.gz
+    wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz -P /tmp/
+    tar -C /usr/local -xzf /tmp/go1.20.6.linux-amd64.tar.gz
+    rm -rf /tmp/go1.20.6.linux-amd64.tar.gz
 fi
+go mod tidy
 
 # 3.Install kubectl
 if [[ -z $(which kubectl) ]]; then
@@ -32,22 +37,27 @@ if [[ -z $(which ko) ]]; then
     rm -rf /tmp/ko_0.15.2_Linux_x86_64.tar.gz
 fi
 
-# 5.Install k3s
-if [[ -z $(which k3s) ]]; then
-    echo "k3s is not installed. Try to install k3s..."
-    curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.28.7+k3s1 sh -
+# 5.Launch k3s and disable LoadBalancer
+PROXY_ENV=""
+if [[ -n ${https_proxy} ]]; then
+    PROXY_ENV="-e HTTP_PROXY=${http_proxy} -e HTTPS_PROXY=${https_proxy} -e NO_PROXY=${no_proxy}"
 fi
-
-# 6.Launch k3s and disable LoadBalancer
-k3s-killall.sh
-nohup k3s server --disable traefik,servicelb > /var/log/k3s.log &
+docker run -itd --network host \
+    --privileged \
+    ${PROXY_ENV} \
+    -v ${HOME}/openapp:/root/openapp \
+    --name k3s rancher/k3s:v1.27.4-k3s1 server --disable traefik,servicelb
 sleep 30
 
+# 6. Copy cluster config out
+mkdir -p ${HOME}/.config/
+docker cp k3s:/etc/rancher/k3s/k3s.yaml ${HOME}/.config/k3s.yaml
+
 # 7. Deploy openapp
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+export KUBECONFIG=${HOME}/.config/k3s.yaml
 ko apply -Rf config
 
 # 8. Echo related information
 echo "Please run following command to control openapp:"
-echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml"
-echo "kubectl get apptemplate -A"
+utils::echo_note "export KUBECONFIG=${HOME}/.config/k3s.yaml"
+utils::echo_note "kubectl get apptemplate -A"
--- hack/utils.sh
+++ hack/utils.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function create_gopath_tree() {
+function utils::create_gopath_tree() {
   # $1: the root path of repo
   local repo_root=$1
   # $2: go path
@@ -16,4 +16,8 @@ function create_gopath_tree() {
   if [[ ! -e "${go_pkg_dir}" || "$(readlink "${go_pkg_dir}")" != "${repo_root}" ]]; then
     ln -snf "${repo_root}" "${go_pkg_dir}"
   fi
+}
+
+function utils::echo_note() {
+  echo -e "\033[33m$1\033[0m"
 }
